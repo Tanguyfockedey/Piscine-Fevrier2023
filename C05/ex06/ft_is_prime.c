02740f64764670c6aa5ef7af61aeca6e75f0dba6
Tanguy Fockedey <tafocked@shi-r6-p4.s19.be>
2023-02-19 16:17:53 +0100

    c05

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -6,7 +6,7 @@
 /*   By: tafocked <tafocked@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 12:32:20 by tfockede          #+#    #+#             */
-/*   Updated: 2023/02/19 12:56:36 by tafocked         ###   ########.fr       */
+/*   Updated: 2023/02/19 13:52:25 by tafocked         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	ft_is_prime(int nb)
 	return (1);
 }
 
-
+/* 
 #include <stdio.h>
 int	main(void)
 {
@@ -38,3 +38,4 @@ int	main(void)
 		i++;
 	}
 }
+ */
--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -1,42 +1,50 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tafocked <tafocked@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/18 15:59:05 by tfockede          #+#    #+#             */
-/*   Updated: 2023/02/19 13:50:29 by tafocked         ###   ########.fr       */
+/*   Created: 2023/02/19 14:44:15 by tafocked          #+#    #+#             */
+/*   Updated: 2023/02/19 16:17:16 by tafocked         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_sqrt(int nb)
+int	ft_is_prime(int nb)
 {
 	int	i;
 
-	i = 1;
-	while (i <= nb && i <= 46340)
+	i = 2;
+	if (nb < 2)
+		return (0);
+	while (i <= nb / i)
 	{
-		if (nb / i == i && nb % i == 0)
-			return (i);
+		if (nb % i == 0)
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb < 3)
+		return (2);
+	while (!ft_is_prime(nb))
+		nb++;
+	return (nb);
 }
 
 /* 
 #include <stdio.h>
 int	main(void)
 {
-	int i = -10;
+	int i = 2147473647;
 
-	printf("La racine carrée de %d vaut %d !\n", 0, ft_sqrt(0));
-
-	while (i <= 100000)
+	while (i < 2147483647)
 	{
-		if (ft_sqrt(i) != 0)
-			printf("La racine carrée de %d vaut %d !\n", i, ft_sqrt(i));
-		i++;
+	printf("%d : %d\n", i, ft_find_next_prime(i));
+	i++;
 	}
 }
  */
