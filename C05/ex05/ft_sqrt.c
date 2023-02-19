112bf6456e9395ab9297cf1017f9fdf44b186988
Tanguy Fockedey <tafocked@shi-r6-p4.s19.be>
2023-02-19 16:41:23 +0100

    ex05

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: tafocked <tafocked@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 15:59:05 by tfockede          #+#    #+#             */
-/*   Updated: 2023/02/19 13:50:29 by tafocked         ###   ########.fr       */
+/*   Updated: 2023/02/19 16:39:49 by tafocked         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int	ft_sqrt(int nb)
 	int	i;
 
 	i = 1;
-	while (i <= nb && i <= 46340)
+	while (nb / i >= i)
 	{
 		if (nb / i == i && nb % i == 0)
 			return (i);
@@ -28,15 +28,16 @@ int	ft_sqrt(int nb)
 #include <stdio.h>
 int	main(void)
 {
-	int i = -10;
+	int i = 2147483647;
 
-	printf("La racine carrée de %d vaut %d !\n", 0, ft_sqrt(0));
+//	printf("La racine carrée de %d vaut %d !\n", i, ft_sqrt(i));
 
-	while (i <= 100000)
+ 	while (i <= 1000000)
 	{
 		if (ft_sqrt(i) != 0)
 			printf("La racine carrée de %d vaut %d !\n", i, ft_sqrt(i));
+//		ft_sqrt(i);
 		i++;
-	}
+	} 
 }
  */
