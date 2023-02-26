ddf0a72400ab19ba5e5ec4ae1e3c78f041e16a84
Tanguy Fockedey <tafocked@fu-r3-p4.s19.be>
2023-02-26 12:49:18 +0100

    shell01

--- Shell_01/ex07/r_dwssap.sh
+++ Shell_01/ex07/r_dwssap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat /etc/passwd | grep -v "#" | sed "s/:.*//" | sed -n 'n;p' | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | perl -pe 's/\n/, / unless eof; s/\n/./'
+cat /etc/passwd | grep -v "#" | sed "s/:.*//" | sed -n 'n;p' | rev | sort -r | sed -n "$FT_LINE1,$FT_LINE2 p" | perl -pe 's/\n/, / unless eof; s/\n/./' | tr -d '\n'
 #                remove comment, remove eol,     every other, rev word, rev list, from ftline1 to ftline2,     all on one line
 #								 cut -d ":" -f1																   p=work like sed, e=execute command
--- Shell_01/ex08/add_chelou.sh
+++ Shell_01/ex08/add_chelou.sh
@@ -6,4 +6,4 @@
 #x3=$(echo $x1 + $x2 | bc)
 #x3=$(echo "obase=13; $x3" | bc) 
 #echo $x3 | tr "0123456789ABC" "gtaio luSnemf"
-echo $FT_NBR1 + $FT_NBR2 | tr "\'\\\\\"\?\!" '01234' | tr 'mrdoc' '01234' | xargs -I{} echo 'obase=13; ibase=5; {}' | bc | tr '0123456789ABC' 'gtaio luSnemf'
+echo $FT_NBR1 + $FT_NBR2 | tr "\'\\\\\"\?\!" '01234' | tr 'mrdoc' '01234' | xargs echo 'obase=13; ibase=5;' | bc | tr '0123456789ABC' 'gtaio luSnemf'
